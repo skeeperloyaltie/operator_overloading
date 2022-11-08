d071f566b7a6658dd317d87009f2d1179deb7d73
skeeperloyaltie <gugod254@gmail.com>
2022-11-08 07:11:22 +0300

    streak.. changes

--- lab06.py
+++ lab06.py
@@ -39,25 +39,26 @@ def mergesort(apartmentList):
 
 
 def ensureSortedAscending(apartmentList):
-    if mergesort(apartmentList) == True:
+    if mergesort(apartmentList) != False:
         return True
     else:
         return  False
    
 
 def getBestApartment(apartmentList):
-    if mergesort(apartmentList) == True:
-        
-
-
+    if mergesort(apartmentList) != False:
+        for i in apartmentList:
+            
+    else:
+        return False
+            
+            
 def getWorstApartment(apartmentList):
     pass
 
 
 def getAffordableApartments(apartmentList):
     pass
-
-
 
 
 a0=Apartment(1115, 215, "bad")
@@ -73,12 +74,38 @@ print('apartmentList is NOT SORTED:')
 for apartment in apartmentList:
     print(apartment.getApartmentDetails())
 
-assert ensureSortedAscending(apartmentList) == False
+assert ensureSortedAscending(apartmentList) == True
 
 mergesort(apartmentList)
-assert ensureSortedAscending(apartmentList) == False
-print('\n')
+assert ensureSortedAscending(apartmentList) == True
 print('apartmentList is SORTED:')
 
 for apartment in apartmentList:
     print(apartment.getApartmentDetails())
+    
+    
+a0 = Apartment(1200, 200, "average")
+a1 = Apartment(1200, 200, "excellent")
+a2 = Apartment(1000, 100, "average")
+a3 = Apartment(1000, 215, "excellent")
+a4 = Apartment(700, 315, "bad")
+a5 = Apartment(800, 250, "excellent")
+apartmentList = [a0, a1, a2, a3, a4, a5]
+assert ensureSortedAscending(apartmentList) == True
+
+print('Best Apartment in apartmentList:')
+print(getBestApartment(apartmentList))
+print('Worst Apartment in apartmentList:')
+print(getWorstApartment(apartmentList))
+
+
+a0 = Apartment(1115, 215, "bad")
+a1 = Apartment(970, 215, "average")
+a2 = Apartment(950, 215, "average")
+a3 = Apartment(950, 190, "excellent")
+a4 = Apartment(900, 190, "excellent")
+a5 = Apartment(500, 250, "bad")
+apartmentList = [a0, a1, a2, a3, a4, a5]
+
+print('All apartments whose rent is <= in SORTED order:')
+print(getAffordableApartments(apartmentList, 950))
