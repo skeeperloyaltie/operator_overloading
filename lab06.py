6f29bc3ccbdd38c9a8590ba51a0c5292f51a8510
W1nterFr3ak <byronchris25@gmail.com>
2022-11-09 09:29:30 +0300

    Done G

--- lab06.py
+++ lab06.py
@@ -2,57 +2,88 @@
 from Apartment import *
 
 
-def mergesort(apartmentList):
-    for apartment, f in enumerate(apartmentList):
-        
-        if apartment > 1:
-            mid = len(apartmentList) // 2
-            lefthalf = apartmentList[:mid]
-            righthalf = apartmentList[mid:]
-            mergesort(lefthalf)
-            mergesort(righthalf)
-            i = 0
-            j = 0
-            k = 0
+# Python program for implementation of MergeSort
 
-            while i < len(lefthalf) and j < len(righthalf):
-                
-                if lefthalf[i].getRent() < righthalf[j].getRent():
-                    apartmentList[k] = lefthalf[i]
-                    i = i + 1
-                else:
-                    apartmentList[k] = righthalf[j]
-                    j = j + 1
-                k = k + 1
-                
-                
-            
-            while i < len(lefthalf):
-                apartmentList[k] = lefthalf[i]
-                i = i + 1
-                k = k + 1
+# Merges two subarrays of arr[].
+# First subarray is arr[l..m]
+# Second subarray is arr[m+1..r]
 
-            while j < len(righthalf):
-                apartmentList[k] = righthalf[j]
-                j = j + 1
-                k = k + 1
-                
+
+def merge(arr, l, m, r):
+    n1 = m - l + 1
+    n2 = r - m
+
+    # create temp arrays
+    L = [0] * (n1)
+    R = [0] * (n2)
+
+    # Copy data to temp arrays L[] and R[]
+    for i in range(0, n1):
+        L[i] = arr[l + i]
+
+    for j in range(0, n2):
+        R[j] = arr[m + 1 + j]
+
+    # Merge the temp arrays back into arr[l..r]
+    i = 0    # Initial index of first subarray
+    j = 0    # Initial index of second subarray
+    k = l    # Initial index of merged subarray
+
+    while i < n1 and j < n2:
+        if L[i].getRent() <= R[j].getRent():
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+
+    # Copy the remaining elements of L[], if there
+    # are any
+    while i < n1:
+        arr[k] = L[i]
+        i += 1
+        k += 1
+
+    # Copy the remaining elements of R[], if there
+    # are any
+    while j < n2:
+        arr[k] = R[j]
+        j += 1
+        k += 1
+
+# l is for left index and r is right index of the
+# sub-array of arr to be sorted
+
+
+def mergesort(arr, l, r):
+    if l < r:
+
+        # Same as (l+r)//2, but avoids overflow for
+        # large l and h
+        m = l+(r-l)//2
+
+        # Sort first and second halves
+        mergesort(arr, l, m)
+        mergesort(arr, m+1, r)
+        merge(arr, l, m, r)
+
 
 
 def ensureSortedAscending(apartmentList):
-    if mergesort(apartmentList) != False:
+    if mergesort(apartmentList,0,len(apartmentList)-1) != False:
         return True
     else:
         return  False
    
 
 def getBestApartment(apartmentList):
-    if mergesort(apartmentList) != False:
+    if mergesort(apartmentList,0,len(apartmentList)-1) != False:
         return apartmentList[0].getApartmentDetails()
             
             
 def getWorstApartment(apartmentList):
-    if mergesort(apartmentList) != False:
+    if mergesort(apartmentList,0,len(apartmentList)-1) != False:
         return apartmentList[-1].getApartmentDetails()
                     
 
@@ -61,9 +92,9 @@ def getAffordableApartments(apartmentList, r):
     u = []
     for i, d in enumerate(apartmentList):
         if d.getRent() <= r:
-            u.append(d.getApartmentDetails())
-    mergesort(u)
-    return "\n".join(u)
+            u.append(d)
+    mergesort(u, 0, len(u)-1)
+    return "\n".join([ x.getApartmentDetails() for x in u])
                
 
 
@@ -82,7 +113,7 @@ for apartment in apartmentList:
 
 assert ensureSortedAscending(apartmentList) == True
 print('\n')
-mergesort(apartmentList)
+mergesort(apartmentList,0,len(apartmentList)-1)
 assert ensureSortedAscending(apartmentList) == True
 print('apartmentList is SORTED:')
 
