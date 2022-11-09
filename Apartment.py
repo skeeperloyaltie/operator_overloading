4314d592cd16fad9b91f09a7290f298f99dd1f2e
W1nterFr3ak <byronchris25@gmail.com>
2022-11-09 21:57:11 +0300

    Done G

--- Apartment.py
+++ Apartment.py
@@ -1,38 +1,36 @@
 class Apartment:
-    def __init__(self, rent, metersfromUCSB, condition):
+    def __init__(self, rent, metersFromUCSB, condition):
         self.rent = rent
-        self.metersfromUCSB = metersfromUCSB
+        self.metersFromUCSB = metersFromUCSB
         self.condition = condition
         
     def getRent(self):
         return self.rent
-    def getMetersfromUCSB(self):
-        return self.metersfromUCSB
+
+    def getMetersFromUCSB(self):
+        return self.metersFromUCSB
     
     def getCondition(self):
         return self.condition
     
     def getApartmentDetails(self):
-        return str('(Apartment): Rent: ${}, Distance from UCSB: {}m, condition: {}'.format(self.rent, self.metersfromUCSB, self.condition))
+        return str('(Apartment): Rent: ${}, Distance from UCSB: {}m, condition: {}'.format(self.rent, self.metersFromUCSB, self.condition))
     
     def __lt__(self, rhs):
-        if self.rent <= rhs.rent:
+        if self.rent < rhs.rent:
             return True
-     
-        else:
-            return False
+        return False
 
     def __eq__(self, rhs):
         if self.rent != rhs.rent:
-            if self.metersfromUCSB == rhs.metersfromUCSB:
+            if self.metersFromUCSB == rhs.metersFromUCSB:
                 if self.condition == rhs.condition:
                     return True
-                else:
-                    return False
+                return False
+            return False
+        return True
 
     def __gt__(self, rhs):
-        if self.rent <= rhs.rent:
+        if self.rent > rhs.rent:
             return True
- 
-        else:
-            return False
+        return False
