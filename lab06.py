3d775e00312414b2e981f026c05acdc3f648ef2e
W1nterFr3ak <byronchris25@gmail.com>
2022-11-09 09:30:28 +0300

    Done G

--- lab06.py
+++ lab06.py
@@ -2,13 +2,6 @@
 from Apartment import *
 
 
-# Python program for implementation of MergeSort
-
-# Merges two subarrays of arr[].
-# First subarray is arr[l..m]
-# Second subarray is arr[m+1..r]
-
-
 def merge(arr, l, m, r):
     n1 = m - l + 1
     n2 = r - m
